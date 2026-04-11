b4342c2e67a09234987d7d2ae4b58597129eca33
abdullatiffozilov20-source <abdullatiffozilov20@gmail.com>
2026-04-11 13:44:53 +0900

    Use environment variable for app port

--- index.js
+++ index.js
@@ -420,4 +420,4 @@ setInterval(() => {
 bot.launch();
 console.log("🤖 Bot is running!");
 
-app.listen(5000, () => console.log("🌐 Web app running on port 5000"));
+app.listen(process.env.PORT || 5000, () => console.log("🌐 Web app running on port 5000"));
