1d94c335ad75fe0f7291cf61fb53efbcd22d0621
abdullatiffozilov20-source <abdullatiffozilov20@gmail.com>
2026-04-11 23:39:25 +0900

    Update app.listen to bind to 0.0.0.0

--- index.js
+++ index.js
@@ -508,4 +508,4 @@ setInterval(() => {
 bot.launch();
 console.log("🤖 Bot is running!");
 
-app.listen(process.env.PORT || 5000, () => console.log("🌐 Web app running on port 5000"));
+app.listen(process.env.PORT || 5000, '0.0.0.0', () => console.log("🌐 Web app running on port 5000"));
